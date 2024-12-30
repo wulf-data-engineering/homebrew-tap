489930a8795d69e631751b0406d3f9a000e45433
christoph-wulf <3455613+christoph-wulf@users.noreply.github.com>
2024-12-30 00:29:39 +0000

    [create-pull-request] automated change

--- Formula/kiek.rb
+++ Formula/kiek.rb
@@ -1,22 +1,24 @@
-class Kiek < Formula
+class kiek < Formula
   version '1.0.0'
+  sha256_mac '00fe0c74ae8379f49f66b399d1ecbff8fda2dff9d4eb10e7e3867cbce472cf43'
+  sha256_linux 'f72fb01d99cc8fef6390bfb8176445d77c54bee65d94b7042a9e30a988a3c5fa'
   desc "kiek helps you to look into Kafka topics."
   homepage "https://github.com/wulf-data-engineering/kiek"
 
   if OS.mac?
-      url "https://github.com/wulf-data-engineering/kiek/releases/download/v1.0.0/kiek-aarch64-apple-darwin.tar.gz"
-      sha256 "51e157a5f5f77cd392da2f92f4e784bd259e1ad8cbed4a6ed203b1bbce6c9ef6"
+      url "https://github.com/wulf-data-engineering/kiek/releases/download/v#{version}/kiek-aarch64-apple-darwin.tar.gz"
+      sha256 "#{sha256_mac}"
   elsif OS.linux?
-      url "https://github.com/wulf-data-engineering/kiek/releases/download/v1.0.0/kiek-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "786ccb9cb649fce2b1e2efdfba5e47c4458f02bacf72a2fd4277cf5bdca5bf31"
+      url "https://github.com/wulf-data-engineering/kiek/releases/download/v#{version}/kiek-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "#{sha256_linux}"
   end
 
   conflicts_with "kiek"
 
   def install
     bin.install "kiek"
-#     bash_completion.install "complete/kiek.bash"
-#     fish_completion.install "complete/kiek.fish"
-#     zsh_completion.install "complete/_kiek"
+    zsh_completion.install "completions/_kiek"
+    bash_completion.install "completions/kiek.bash"
+    fish_completion.install "completions/kiek.fish"
   end
 end

b58f01eb9ea4360802962567a25096ad2e70006a
Christoph Wulf <christoph.wulf@talentformation.com>
2025-04-22 14:17:18 +0200

    Update formula for kiek 1.2.0 (#8)
    Co-authored-by: christoph-wulf <3455613+christoph-wulf@users.noreply.github.com>

--- Formula/kiek.rb
+++ Formula/kiek.rb
@@ -1,14 +1,14 @@
 class Kiek < Formula
-  version '1.1.2'
+  version '1.2.0'
   desc "kiek helps you to look into Kafka topics."
   homepage "https://github.com/wulf-data-engineering/kiek"
 
   if OS.mac?
       url "https://github.com/wulf-data-engineering/kiek/releases/download/v#{version}/kiek-aarch64-apple-darwin.tar.gz"
-      sha256 "174771eb40ff2a9b593ac48bd1eb6f12d6fb2895c7c58599ea1e8bf4396bc1f5"
+      sha256 "2ff8042f0be26ea9c14ae2e9f013167fd8c7378a6d91431622b7dc09b43fdc5e"
   elsif OS.linux?
       url "https://github.com/wulf-data-engineering/kiek/releases/download/v#{version}/kiek-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "77a7b04d423736233613efe6d57ea1f430a168c83d68023e1f0d9b9d925e508f"
+      sha256 "0d9e543db994c3ef4c0b2bf5b56c41866d49a11033e56af33b7247056afbea48"
   end
 
   conflicts_with "kiek"
